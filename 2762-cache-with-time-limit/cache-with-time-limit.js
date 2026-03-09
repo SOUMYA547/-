5de768c60f921b741324fe40226c2ec8575708ab
SOUMYAKANTA MAHAPATRA <manaswininayak616@gmail.com>
2026-03-10 00:39:53 +0530

    Time: 50 ms (47.89%) | Memory: 53.8 MB (50.14%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,42 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration
+ * @return {boolean}
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    
+    const exists = this.cache.has(key);
+
+    if (exists) {
+        clearTimeout(this.cache.get(key).timer);
+    }
+
+    const timer = setTimeout(() => {
+        this.cache.delete(key);
+    }, duration);
+
+    this.cache.set(key, { value, timer });
+
+    return exists;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if (!this.cache.has(key)) return -1;
+    return this.cache.get(key).value;
+};
+
+/** 
+ * @return {number}
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
