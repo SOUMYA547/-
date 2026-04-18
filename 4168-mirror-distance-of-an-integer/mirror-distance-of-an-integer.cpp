19a45eb27418958c2881789518665ce92725eaee
SOUMYAKANTA MAHAPATRA <manaswininayak616@gmail.com>
2026-04-18 16:18:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (60.81%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -6,10 +6,10 @@ public:
         }
         int a = n;
         int b = 0;
-        while(n > 0){
-        int d = n % 10;
-        b = b * 10 + d;
-        n /= 10;
+        while( n > 0){
+            int d = n % 10;
+            b = b * 10 + d;
+            n /= 10;
         }
         return abs(a - b);
     }
