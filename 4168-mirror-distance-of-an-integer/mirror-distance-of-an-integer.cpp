fce50b6c4871019b5dfd03fe885d5bbb070927ac
SOUMYAKANTA MAHAPATRA <manaswininayak616@gmail.com>
2026-04-18 16:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (86.21%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        if(n < 0){
+            return 0;
+        }
+        int a = n;
+        int b = 0;
+
+        while(n > 0){
+            int d = n % 10;
+            b = b * 10 + d;
+            n /= 10;
+        }
+        return abs(a - b);
+    }
+};
