044c14abd128179981b5a85866721974711e9ebf
SOUMYAKANTA MAHAPATRA <manaswininayak616@gmail.com>
2026-09-18 00:05:50 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (80.33%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
@@ -4,7 +4,7 @@ public:
         int a = nums.size();
         int b = 0;
         for(int i = 0; i < a; i++){
-            if(__builtin_popcount(i) == k){
+            if (__builtin_popcount(i) == k){
                 b += nums[i];
             }
         }
